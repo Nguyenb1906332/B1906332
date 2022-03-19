cefce9fdaab7bcf7619a2e59474b97f4f4f96538
Nguyenb1906332 <101681747+Nguyenb1906332@users.noreply.github.com>
2022-03-19 17:05:33 +0700

    Add files via upload

---
+++ formsuamk.php
@@ -0,0 +1,15 @@
+<!DOCTYPE HTML>
+<html>  
+<body>
+
+<form action="checkmk.php" method="post">
+Mat khau cu: <input type="password" name="pass"><br>
+Mat khau moi: <input type="password" name="newpass"><br>
+Xac nhan lai MK: <input type="password" name="newpass"><br>
+
+<input type="submit">
+
+</form>
+
+</body>
+</html>
